a4175956cd4a2afbcf5ed7ec698b41250eaa8718
qiang <null.null.null@qq.com>
2024-03-31 18:32:05 +0800

    update sample

--- sample/cli.py
+++ sample/cli.py
@@ -1,6 +1,7 @@
-from stable_diffusion_cpp import StableDiffusion
+from stable_diffusion_cpp.stable_diffusion_cpp import StableDiffusion
 from PIL import Image
 import numpy as np
+import argparse
 
 
 good_words = [
@@ -10,11 +11,15 @@ good_words = [
     "masterpiece",
 ]
 
+parser = argparse.ArgumentParser(description='python cli.py -m mode_path -p prompt')
+parser.add_argument("-m", required= True)
+parser.add_argument("-p", required= True)
+args = parser.parse_args()
+
 sd = StableDiffusion(
-    model_path="",
-    esrgan_path="",
+    model_path=args.m,
 )
-img = sd.txt_to_img(prompt=good_words + [""])
+img = sd.txt_to_img(prompt=good_words + [args.p])
 img = sd.upscale_img(img, 4)
 im = Image.fromarray(np.array(img))
 im.save("img.jpg")
